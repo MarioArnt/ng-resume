5c92a2bb43de917a35e241333686ad36259115cd
Mario Arnautou <mario@arnautou.fr>
2015-11-24 12:55:21 +0100

    Responsive design

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,42 @@
 'use strict';
 
 angular.module('ngResume')
-.controller('MainCtrl', function ($scope, $location, profile, $rootScope) {
+.controller('MainCtrl', function ($scope, $location, $http, profile, $window, $rootScope) {
   $scope.loadingFinished = true;
   $scope.me = profile.data;
   $rootScope.title = $scope.me.fullName + ' | Interactive résumé';
   $scope.$on('onRepeatLast', function(){
-    $('.skills-list').columnize({columns: 3, lastNeverTallest:true});
+    columnizeSkills();
   });
+
+  angular.element($window).bind('resize', function () {
+    if($('.dontsplit').parent().is('.column')) {
+      $('.dontsplit').unwrap();
+      columnizeSkills();
+    }
+  });
+
+  function columnizeSkills() {
+    console.log($window.innerWidth);
+    if($window.innerWidth < 768) {
+      $('.skills-list').columnize({columns: 1, lastNeverTallest:true});
+      return;
+    }
+    else if($window.innerWidth > 768 && $window.innerWidth < 992) {
+      $('.skills-list').columnize({columns: 2, lastNeverTallest:true});
+      return;
+    }
+    else if($window.innerWidth > 992){
+      $('.skills-list').columnize({columns: 3, lastNeverTallest:true});
+      return;
+    }
+    $('.skills-list').columnize({columns: 1, lastNeverTallest:true});
+  }
+
+  $scope.me.events.forEach(function(event){
+    event.showMore = false;
+  });
+
   $scope.me.projects.forEach(function(project){
     project.skills.forEach(function(skill) {
       skill.tooltip = {
@@ -19,20 +48,27 @@ angular.module('ngResume')
     if(lang === $scope.me.lang) {return;}
     $location.path('/'+lang);
   };
-  $scope.sendMail = function() {
-    if(!$scope.mail.from){
-      console.log('please enter a valid email address');
+  $scope.emptyOptionalFields = false;
+  $scope.sendMail = function(isValid) {
+    if (isValid) {
+      if(!$scope.mail.object || !$scope.mail.body){
+        $scope.emptyOptionalFields = !$scope.emptyOptionalFields;
         return;
+      }
+      $http({
+         method: 'POST',
+         url: '/mail',
+         data: $.param({
+             from: $scope.mail.from,
+             object: $scope.mail.object,
+             content: $scope.mail.body
+         }),
+         headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      }).then(function() {
+        console.log('success');
+      }, function(){
+        console.log('failure');
+      });
     }
-    if(!$scope.mail.object){
-      console.log('no object, send anyway ?');
-      return;
-    }
-    if(!$scope.mail.body){
-      console.log('empty mail, send anyway ?');
-      return;
-    }
-    console.log('everything seems allright, sending POST request');
-    //send post request
   };
 });
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var bodyParser = require("body-parser");
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
 var app  = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req, res) {
@@ -20,6 +25,26 @@ app.get('/download/cv/fr', function(req, res){
 app.get('/download/cv/es', function(req, res){
   var file = __dirname + '/dl/CV-es.pdf';
   res.download(file);
+});
+
+app.post('/mail', function(req, res){
+  var transporter = nodemailer.createTransport(smtpTransport({
+      host: 'localhost',
+      port: 465,
+  }));
+  transporter.sendMail({
+    from: req.body.from,
+    to: "mario.arnautou@gmail.com",
+    subject: req.body.object,
+    text: req.body.content
+  }, function(error, info){
+    if(error){
+      console.log(error);
+    }
+    else{
+      console.log('Message sent: ' + info.response);
+    }
+  });
 });
 
 app.listen(8080);
