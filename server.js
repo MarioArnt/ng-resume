f2bd45adabdddad7ee6188740df582697c3f9ceb
Mario Arnautou <mario@arnautou.fr>
2018-07-09 11:36:05 +0200

    Use the port dynamically sets

--- server.js
+++ server.js
@@ -71,4 +71,6 @@ function verifyRecaptcha(key, callback) {
   });
 }
 
-app.listen(8081);
+app.listen(process.env.PORT || 8081, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
